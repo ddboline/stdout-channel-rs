e4b11777782d439c387339a5911398d6c4e455b1
Daniel Boline <ddboline@gmail.com>
2021-03-13 12:40:52 -0500

    bump version, move stack-string dependency to dev only

--- src/lib.rs
+++ src/lib.rs
@@ -10,43 +10,47 @@
 
 use anyhow::Error;
 use deadqueue::unlimited::Queue;
-use stack_string::StackString;
 use std::{fmt, ops::Deref, sync::Arc};
+use std::fmt::Display;
 use tokio::{
     io::{stderr, stdout, AsyncWriteExt},
     sync::Mutex,
     task::{spawn, JoinHandle},
 };
 
-enum MessageType {
-    Mesg(StackString),
+enum MessageType<T> {
+    Mesg(T),
     Close,
 }
 
-type ChanType = Queue<MessageType>;
+type ChanType<T> = Queue<MessageType<T>>;
 type TaskType = JoinHandle<Result<(), Error>>;
 
 #[derive(Clone)]
-pub struct StdoutChannel {
-    stdout_queue: Arc<ChanType>,
-    stderr_queue: Arc<ChanType>,
+pub struct StdoutChannel<T> {
+    stdout_queue: Arc<ChanType<T>>,
+    stderr_queue: Arc<ChanType<T>>,
     stdout_task: Arc<Mutex<Option<TaskType>>>,
     stderr_task: Arc<Mutex<Option<TaskType>>>,
 }
 
-impl Default for StdoutChannel {
+impl<T> Default for StdoutChannel<T>
+where T: Display + Send + 'static
+{
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl fmt::Debug for StdoutChannel {
+impl<T> fmt::Debug for StdoutChannel<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(f, "StdoutChannel")
     }
 }
 
-impl StdoutChannel {
+impl<T> StdoutChannel<T>
+where T: Display + Send + 'static
+{
     pub fn new() -> Self {
         let stdout_queue = Arc::new(Queue::new());
         let stderr_queue = Arc::new(Queue::new());
@@ -66,7 +70,7 @@ impl StdoutChannel {
         }
     }
 
-    pub fn with_mock_stdout(mock_stdout: MockStdout, mock_stderr: MockStdout) -> Self {
+    pub fn with_mock_stdout(mock_stdout: MockStdout<T>, mock_stderr: MockStdout<T>) -> Self {
         let stdout_queue = Arc::new(Queue::new());
         let stderr_queue = Arc::new(Queue::new());
         let stdout_task = Arc::new(Mutex::new(Some(spawn({
@@ -85,11 +89,11 @@ impl StdoutChannel {
         }
     }
 
-    pub fn send<T: Into<StackString>>(&self, item: T) {
+    pub fn send(&self, item: impl Into<T>) {
         self.stdout_queue.push(MessageType::Mesg(item.into()));
     }
 
-    pub fn send_err<T: Into<StackString>>(&self, item: T) {
+    pub fn send_err(&self, item: impl Into<T>) {
         self.stderr_queue.push(MessageType::Mesg(item.into()));
     }
 
@@ -105,21 +109,21 @@ impl StdoutChannel {
         Ok(())
     }
 
-    async fn stdout_task(queue: &ChanType) -> Result<(), Error> {
+    async fn stdout_task(queue: &ChanType<T>) -> Result<(), Error> {
         while let MessageType::Mesg(line) = queue.pop().await {
             stdout().write_all(format!("{}\n", line).as_bytes()).await?;
         }
         Ok(())
     }
 
-    async fn stderr_task(queue: &ChanType) -> Result<(), Error> {
+    async fn stderr_task(queue: &ChanType<T>) -> Result<(), Error> {
         while let MessageType::Mesg(line) = queue.pop().await {
             stderr().write_all(format!("{}\n", line).as_bytes()).await?;
         }
         Ok(())
     }
 
-    async fn mock_stdout(queue: &ChanType, mock_stdout: &MockStdout) -> Result<(), Error> {
+    async fn mock_stdout(queue: &ChanType<T>, mock_stdout: &MockStdout<T>) -> Result<(), Error> {
         while let MessageType::Mesg(line) = queue.pop().await {
             mock_stdout.lock().await.push(line);
         }
@@ -128,22 +132,22 @@ impl StdoutChannel {
 }
 
 #[derive(Clone)]
-pub struct MockStdout(Arc<Mutex<Vec<StackString>>>);
+pub struct MockStdout<T>(Arc<Mutex<Vec<T>>>);
 
-impl Default for MockStdout {
+impl<T> Default for MockStdout<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl Deref for MockStdout {
-    type Target = Mutex<Vec<StackString>>;
+impl<T> Deref for MockStdout<T> {
+    type Target = Mutex<Vec<T>>;
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-impl MockStdout {
+impl<T> MockStdout<T> {
     pub fn new() -> Self {
         Self(Arc::new(Mutex::new(Vec::new())))
     }
@@ -152,13 +156,14 @@ impl MockStdout {
 #[cfg(test)]
 mod tests {
     use anyhow::Error;
+    use stack_string::StackString;
 
     use super::{MockStdout, StdoutChannel};
 
     #[tokio::test]
     async fn test_default_mockstdout() -> Result<(), Error> {
         let mock = MockStdout::default();
-        mock.lock().await.push("HEY".into());
+        mock.lock().await.push(StackString::from("HEY"));
         assert_eq!(mock.lock().await.len(), 1);
         assert_eq!(mock.lock().await[0].as_str(), "HEY");
         Ok(())
@@ -166,10 +171,10 @@ mod tests {
 
     #[tokio::test]
     async fn test_default() -> Result<(), Error> {
-        let chan = StdoutChannel::default();
+        let chan = StdoutChannel::<StackString>::default();
 
         chan.send("stdout: Hey There");
-        chan.send("What's happening".to_string());
+        chan.send("What's happening");
         chan.send_err("stderr: How it goes");
 
         chan.close().await?;
@@ -178,10 +183,10 @@ mod tests {
 
     #[tokio::test]
     async fn test_stdout_task() -> Result<(), Error> {
-        let chan = StdoutChannel::new();
+        let chan = StdoutChannel::<StackString>::default();
 
         chan.send("stdout: Hey There");
-        chan.send("What's happening".to_string());
+        chan.send("What's happening");
         chan.send_err("stderr: How it goes");
 
         chan.close().await?;
@@ -190,13 +195,13 @@ mod tests {
 
     #[tokio::test]
     async fn test_mock_stdout() -> Result<(), Error> {
-        let stdout = MockStdout::new();
+        let stdout = MockStdout::<StackString>::new();
         let stderr = MockStdout::new();
 
         let chan = StdoutChannel::with_mock_stdout(stdout.clone(), stderr.clone());
 
         chan.send("stdout: Hey There");
-        chan.send("What's happening".to_string());
+        chan.send("What's happening");
         chan.send_err("stderr: How it goes");
         chan.close().await?;
 
